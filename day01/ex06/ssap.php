0935891b458ac9ccfa67771a7ce682d69a143896
abduraghmaan GABRIELS <agabrie@c2r3s3.wethinkcode.co.za>
2018-10-03 16:14:32 +0200

    did ex06

--- day01/ex06/ssap.php
+++ day01/ex06/ssap.php
@@ -1,5 +1,22 @@
 #!/usr/bin/php
 <?php
-    $args = $argv;
-    foreach()
+    function ft_split($string)
+    {
+        $full = preg_split('/\s+/', $string);
+        sort($full);
+        return($full);
+    }
+    $i = 0;
+    for($num = 1; $num < $argc; $num++)
+    {
+        $stuff = ft_split($argv[$num]);
+        foreach($stuff as $things)
+        {
+            $arr[$i] = $things;
+            $i++;
+        }
+    }
+    sort($arr);
+    foreach($arr as $elem)
+        echo $elem."\n";
 ?>
