9e85f0b62cfa1bea6209baada094d99cdc8c0959
abduraghmaan GABRIELS <agabrie@c1r2s1.wethinkcode.co.za>
2018-10-05 17:31:16 +0200

    fixing small discrepancies in cookie_crisp

--- day03/ex03/cookie_crisp.php
+++ day03/ex03/cookie_crisp.php
@@ -1,20 +1,22 @@
 <?php
-if($_GET)
+if($name = $_GET["name"])
 {
-		switch($_GET["action"])
-		{
-			case "set":
-				setcookie($_GET["name"], $_GET["value"], time() + 3600);
-				break;
-			case "get":
-				if($_COOKIE[$_GET["name"]] == $_GET["name"])
-					echo $_COOKIE[$_GET["name"]]. "\n";
-				break;
-			case "del":
-				if($_COOKIE[$_GET["name"]] == $_GET["name"])
-					setcookie($_GET["name"],NULL, -1);
-			default:
-				break;
-		}
+switch($_GET["action"])
+{
+	case "set":
+		if($name)
+			setcookie($name, $_GET["value"], time() + 300);
+		break;
+	case "get":
+		if($_COOKIE[$name])
+			echo $_COOKIE[$name]."\n";
+		break;
+	case "del":
+		if($name)
+			setcookie($name,NULL, time() - 300);
+		break;
+	default:
+		break;
+}
 }
 ?>
