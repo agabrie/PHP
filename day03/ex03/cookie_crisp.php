9af8c5f80a3afd9e08d6c32a8f1407e7482130c4
abduraghmaan GABRIELS <agabrie@c1r2s1.wethinkcode.co.za>
2018-10-05 18:02:00 +0200

    cookie crisp normed

--- day03/ex03/cookie_crisp.php
+++ day03/ex03/cookie_crisp.php
@@ -1,22 +1,22 @@
 <?php
 if($name = $_GET["name"])
 {
-switch($_GET["action"])
-{
-	case "set":
-		if($name)
-			setcookie($name, $_GET["value"], time() + 300);
-		break;
-	case "get":
-		if($_COOKIE[$name])
-			echo $_COOKIE[$name]."\n";
-		break;
-	case "del":
-		if($name)
-			setcookie($name,NULL, time() - 300);
-		break;
-	default:
-		break;
-}
+	switch($_GET["action"])
+	{
+		case "set":
+			if($name)
+				setcookie($name, $_GET["value"], time() + 300);
+			break;
+		case "get":
+			if($_COOKIE[$name])
+				echo $_COOKIE[$name]."\n";
+			break;
+		case "del":
+			if($name)
+				setcookie($name,NULL, time() - 300);
+			break;
+		default:
+			break;
+	}
 }
 ?>
