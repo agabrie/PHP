8355ea8c4f4625b6761e573ba780656abd0b9e7e
abduraghmaan GABRIELS <agabrie@c1r2s1.wethinkcode.co.za>
2018-10-07 15:26:19 +0200

    finished add_item.php

--- rush00/additem.php
+++ rush00/additem.php
@@ -1,13 +1,38 @@
 <?php
-	if($_GET["submit"] === "OK")
+	$itemdir = "itemsdir";
+	$itemfile = $itemdir."/items";
+	if($_POST["submit"] === "OK")
 	{
-		
+		if(!file_exists($itemdir))
+			mkdir($itemdir);
+		if(file_exists($itemfile))
+		{
+			$array =  unserialize(file_get_contents($itemfile));
+			foreach($array as $item)
+			{
+				if($item["id"] === $_POST["id"])
+				{
+					echo "Item already exists";
+					return;
+				}
+			}
+		}
+		$info["id"] = $_POST["id"];
+		$info["img"] = $_POST["img"];
+		$info["desc"] = $_POST["desc"];
+		$info["type"] = $_POST["type"];
+		$info["cat"] = $_POST["cat"];
+		$info["price"] = $_POST["price"];
+		$info["amount"] = $_POST["amount"];
+		$array[] = $info;
+		file_put_contents($itemfile, serialize($array));
+		echo "Item: ".$info["id"]." x ".$info["amount"]." added at R ".$info["price"]." each!\n";
 	}
 ?>
 
 <html><body>
 	<form action="additem.php" method="post">
-		Name:<br><input type="text" name="item"><br>
+		Name:<br><input type="text" name="id"><br>
 		Image:<br><input type="text" name="img"><br>
 		Description:<br><input type="text" name="desc"><br>
 		Type:<br><input type="text" name="type"><br>
