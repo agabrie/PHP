9a570ec55e4e565cd6d117f9d8ede7cb35b1c350
abduraghmaan GABRIELS <agabrie@c1r2s1.wethinkcode.co.za>
2018-10-07 19:14:02 +0200

    addtocart

---
+++ rush00/addtocart.php
@@ -0,0 +1,25 @@
+<?php
+	function($item, $amount)
+	{
+		$itemdir = "itemsdir";
+		$itemfile = $itemdir."/items";
+		$cartdir = "cartdir";
+		$cartfile = $itemdir."/cart";
+		if(file_exists($itemfile) && file_exists($cartfile))
+		{
+			$array = unserialize(file_get_contents($itemfile));
+			$basket = unserialize(file_get_contents($cartfile));
+			foreach($array as &$elem)
+			{
+				if($item === $elem["id"] && intval($elem["amount"]) - $amount > 0)
+				{
+					$cart["id"] = $elem["id"];
+					$cart["amount"] = $elem["amount"];
+					$elem["amount"] = (intval($elem["amount"])-$amount)."";
+				}
+			}
+			$basket[] = $cart;
+			file_put_contents($cartfile, serialize($basket));
+		}
+	}
+?>
